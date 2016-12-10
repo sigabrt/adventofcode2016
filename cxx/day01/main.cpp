e81ab36de28e60fbe478784c5ce108aa092899c4
Alex Meyer <alexander.dude.meyer@gmail.com>
2016-12-09 20:19:19 -0600

    Possibly this will help older clang with GCC headers? I love Travis.

--- cxx/day01/main.cpp
+++ cxx/day01/main.cpp
@@ -5,8 +5,10 @@
 #include <iterator>
 #include <fstream>
 #include <numeric>
+#include <set>
 #include <sstream>
 #include <vector>
+#include <unordered_map>
 
 enum direction {
   kNorth = 0,
@@ -46,10 +48,30 @@ struct command {
 struct state {
   struct coord {
     int x, y;
+
+    static std::vector<coord> interpolate(const coord &start, const coord &end) {
+      std::vector<coord> results;
+      if (start.x != end.x) {
+        for (int i = start.x; (start.x - end.x > 0) ? (i >= end.x) : (i <= end.x); (start.x - end.x > 0) ? --i : ++i) {
+          results.push_back(coord{i, start.y});
+        }
+      } else {
+        for (int i = start.y; (start.y - end.y > 0) ? (i >= end.y) : (i <= end.y); (start.y - end.y > 0) ? --i : ++i) {
+          results.push_back(coord{start.x, i});
+        }
+      }
+      return results;
+    }
   };
 
   direction dir;
   coord pos;
+
+  bool operator<(const state &other) const {
+    if (this->pos.x < other.pos.x) return true;
+    else if (this->pos.y < other.pos.y) return true;
+    return false;
+  }
 
   static coord move(coord cur, direction dir, int steps) {
     switch (dir) {
@@ -70,9 +92,20 @@ struct state {
     direction new_dir{command::turn(cur.dir, cmd.action)};
     return state{new_dir, move(cur.pos, new_dir, cmd.steps)};
   }
+
+  static std::vector<state> interpolate(const state &start, state &end) {
+    auto coords = coord::interpolate(start.pos, end.pos);
+    std::vector<state> states;
+    std::transform(coords.begin(), coords.end(), std::back_inserter(states),
+                   [=](coord &c) {
+                     return state{start.dir, c};
+                   });
+    states.back().dir = end.dir;
+    return states;
+  }
 };
 
-int main(int argc, char *argv[]) {
+int main(int, char *[]) {
   std::ifstream infile{INPUT_DIR "day01.txt"};
   std::vector<std::string> tokens(std::istream_iterator<std::string>{infile},
                                   std::istream_iterator<std::string>{});
@@ -81,7 +114,30 @@ int main(int argc, char *argv[]) {
 
   // Part 1
   state final_state = std::accumulate(commands.begin(), commands.end(), state{kNorth, {0, 0}}, &state::act);
-  std::cout << final_state.pos.x + final_state.pos.y << std::endl;
+  std::cout << "The Easter Bunny HQ is " << final_state.pos.x + final_state.pos.y << " blocks away" << std::endl;
+
+  // Part 2
+  std::vector<state> states = std::accumulate(commands.begin(), commands.end(),
+                                              std::vector<state>{state{kNorth, {0, 0}}},
+                                              [](std::vector<state> s, command c) {
+                                                state new_state = state::act(s.back(), c);
+                                                auto walk = state::interpolate(s.back(), new_state);
+                                                s.insert(s.end(), std::next(walk.begin()), walk.end());
+                                                return s;
+                                              });
+
+  std::set<state> state_set;
+  state first_duplicate_state;
+  std::find_if(states.begin(), states.end(), [&](const state &s) {
+    auto insertion_result = state_set.insert(s);
+    if (!insertion_result.second) {
+      first_duplicate_state = s;
+      return true;
+    }
+    return false;
+  });
+  std::cout << "The Easter Bunny HQ is " << first_duplicate_state.pos.x + first_duplicate_state.pos.y
+            << " blocks away" << std::endl;
 
   return 0;
 }
