6fe747db56cbade1dbb78d72014e6d635fc88be5
Alex Meyer <alexander.dude.meyer@gmail.com>
2016-12-24 18:15:02 -0500

    Day three part two.

--- cxx/day03/main.cpp
+++ cxx/day03/main.cpp
@@ -25,10 +25,29 @@ struct triangle {
   }
 };
 
+std::vector<triangle> from_strings(const std::vector<std::string> &strings) {
+  std::vector<triangle> triangles;
+
+  std::vector<std::vector<int>> sides;
+  std::transform(strings.begin(), strings.end(), std::back_inserter(sides), [](auto s) {
+    return split<int>(s);
+  });
+
+  for (std::size_t i = 0; i < sides.size(); ++i) {
+    triangles.push_back({{sides[i][0], sides[i][3], sides[i][6]}});
+    triangles.push_back({{sides[i][1], sides[i][4], sides[i][7]}});
+    triangles.push_back({{sides[i][2], sides[i][5], sides[i][8]}});
+  }
+
+  return triangles;
+}
+
 int main(int, char *[]) {
   std::ifstream infile{INPUT_DIR "day03.txt"};
-  std::vector<std::string> lines(std::istream_iterator<line>{infile},
-                                 std::istream_iterator<line>{});
+  std::vector<std::string> lines{std::istream_iterator<line>{infile},
+                                 std::istream_iterator<line>{}};
+
+  // Part 1
   std::vector<triangle> triangles;
   std::transform(lines.begin(), lines.end(), std::back_inserter(triangles), &triangle::from_string);
 
@@ -37,5 +56,19 @@ int main(int, char *[]) {
                                     return n + (triangle::validate(t) ? 1 : 0);
                                   });
   std::cout << num_valid << " valid triangles in input" << std::endl;
+
+  // Part 2
+  infile.clear();
+  infile.seekg(0, std::ios::beg);
+  std::vector<std::string> line_triples{std::istream_iterator<n_lines<3>>{infile},
+                                        std::istream_iterator<n_lines<3>>{}};
+  triangles = from_strings(line_triples);
+
+  num_valid = std::accumulate(triangles.begin(), triangles.end(), 0,
+                              [](int n, const triangle &t) {
+                                return n + (triangle::validate(t) ? 1 : 0);
+                              });
+  std::cout << num_valid << " valid triangles in input" << std::endl;
+
   return 0;
 }
--- cxx/utility.h
+++ cxx/utility.h
@@ -1,5 +1,8 @@
 #pragma once
+#include <numeric>
 #include <string>
+#include <sstream>
+#include <vector>
 
 struct line {
   std::string data;
@@ -9,7 +12,51 @@ struct line {
   }
 };
 
-std::istream& operator>>(std::istream& str, line& data) {
-  std::getline(str, data.data);
-  return str;
+template<unsigned int N>
+struct n_lines {
+  static constexpr unsigned int num_lines = N;
+  std::vector<std::string> data;
+
+  operator std::string() const {
+    return std::accumulate(data.begin(), data.end(), std::string(),
+                           [](std::string acc, const std::string &s) {
+                             return acc + "\n" + s;
+                           });
+  }
+};
+
+std::istream& operator>>(std::istream& stream, line& data) {
+  std::getline(stream, data.data);
+  return stream;
+}
+
+template<unsigned int N>
+std::istream& operator>>(std::istream& stream, n_lines<N>& data) {
+  data.data.clear();
+  std::string line;
+  for (unsigned int i = 0; i < data.num_lines; ++i) {
+    std::getline(stream, line);
+    data.data.push_back(line);
+  }
+  return stream;
+}
+
+template <typename T>
+std::vector<T> split(const std::string &str) {
+  std::vector<T> strings;
+  std::istringstream stream(str);
+  T tmp;
+  while (!stream.eof()) {
+    stream >> tmp;
+    strings.push_back(tmp);
+  }
+  return strings;
+}
+
+template <typename InIter, typename OutIter, typename Fn>
+void flatmap(InIter begin, InIter end, OutIter out, Fn fn) {
+  for (; begin != end; ++begin) {
+    auto y = fn(*begin);
+    std::copy(std::begin(y), std::end(y), out);
+  }
 }
